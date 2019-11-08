8bc4d7b3ca8d0b5754e1ce5ee25b39c196c4b825
Dharmesh <dharmeshpadhiyar77@gmail.com>
2019-11-08 20:08:40 +0530

    New screens added...

---
+++ app/src/main/java/dharmesh/padhiyar/swoosh/BaseActivity.kt
@@ -0,0 +1,45 @@
+package dharmesh.padhiyar.swoosh
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+    }
+}
